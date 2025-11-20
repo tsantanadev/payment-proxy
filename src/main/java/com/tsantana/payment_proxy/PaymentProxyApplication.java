2eed3953063bfa3d010c166bfe5e2e699ccef78c
Thiago Santana <thiago91098@gmail.com>
2025-11-20 18:12:39 -0300

    Project created

---
+++ src/main/java/com/tsantana/payment_proxy/PaymentProxyApplication.java
@@ -0,0 +1,13 @@
+package com.tsantana.payment_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsantana/payment_proxy/PaymentProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsantana.payment_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
